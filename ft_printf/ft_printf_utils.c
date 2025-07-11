a2d31f26a60410b746c137daa86490f1e1c2bf05
dwotsche <dwotsche@student.42wolfsburg.de>
2025-07-11 12:04:26 +0200

    fix all errors till now

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,39 +6,25 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 11:12:08 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/07/10 13:41:00 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:03:17 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-static int	ft_extension(unsigned int nb, char *str, int i);
-
 int	ft_putnbr_u(unsigned int nb)
 {
 	int		i;
 	char	str[11];
+	int	ret;
 
 	i = 0;
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		nb = -nb;
-	}
-	else if (nb == 0)
+	if (nb == 0)
 	{
 		write(1, "0", 1);
 		return (1);
 	}
-	return (ft_extension(nb, str, i));
-}
-
-static int	ft_extension(unsigned int nb, char *str, int i)
-{
-	int	ret;
-
-	ret = 0;
 	while (nb > 0)
 	{
 		str[i++] = nb % 10 + 48;
@@ -46,9 +32,10 @@ static int	ft_extension(unsigned int nb, char *str, int i)
 	}
 	ret = i;
 	i--;
-	while (str[i] >= '0' && str[i] <= '9')
+	while (i >= 0)
 	{
-		write(1, &str[i--], 1);
+		write(1, &str[i], 1);
+		i--;
 	}
 	return (ret);
 }
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,25 +6,25 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 16:22:19 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/07/10 17:11:32 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/07/11 11:57:19 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft/libft.h"
+#include "libft/libft.h"
 #include "ft_printf.h"
 
 int	main()
 {
-	char c = 'a';
-	char *s = "test";
-	int i = 187;
-	unsigned int u = 4294967295;
-	char* p = "pointer";
+	// char c = 'a';
+	// char *s = "test";
+	// int i = 187;
+	// unsigned int u = 4294967295;
+	// char* p = "pointer";
 
 	// ft_printf("----- Own Solution -----\n");
 	// ft_printf("Length: %d\n", ft_printf("Char:\t%c\nStr:\t%s\nInt:\t%i\nDec:\t%d\nUI:\t%u\np:\t%p\nx:\t%x\nX:\t%X\nper: %%\n", c, s, i, i, u, p));
-	ft_printf("%s", NULL);
-	printf("%s", NULL);
+	ft_printf("%s", (char *)NULL);
+	printf("%s", (char *)NULL);
 	// printf("----- Printf Solution -----\n");
 	// printf("Length: %d\n", printf("Char:\t%c\nStr:\t%s\nInt:\t%i\nDec:\t%d\nUI:\t%u\np:\t%p\nx:\t%x\nX:\t%X\nper:\t%%\nNULL:\t%s\n", c, s, i, i, u, &p, 0xFF, 255, (char *)NULL));
 }
