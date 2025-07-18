707f3a3f99032192d39cb05110b446bd4831effd
Dennis <dennis.wt20@gmail.com>
2025-07-18 13:31:01 +0200

    add utils, comment etc

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,19 +6,37 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 13:01:06 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/07/18 12:31:18 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/07/18 13:28:11 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// 1. get_next_line call
+// rest leer
+// buffer leer
+// line leer
+// 2. buffer liest gibt an rest
+// es wird in rest geschaut ob sich ein \n befindet
+// wenn ja: in line speichern und zurückgeben
+// wenn nein: wieder buffer liest und addiert zu rest dazu
+// bis ende
+
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	int	read_bytes;
+	char		buffer[BUFFER_SIZE + 1];
+	static char	*rest;
+	int			read_bytes;
+	int			found_nl;
 
-	while (read_bytes = read(fd, buffer, BUFFER_SIZE) > 0)
+	rest = "";
+	while ((read_bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		
+		buffer[read_bytes + 1] = 0;
+		ft_strlcat(rest, buffer, ft_strlen(buffer));
+		if (found_nl = ft_strchr(rest, '\n') && ft_strchr(rest, '\n') != NULL)
+		{
+			
+		}
 	}
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,69 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 13:33:35 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/07/17 13:33:36 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/07/18 13:29:35 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len_dst;
+	size_t	len_src;
+
+	i = 0;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size <= len_dst)
+		return (len_src + size);
+	while (((len_dst + i) < (size - 1)) && src[i] != '\0')
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + i] = '\0';
+	return (len_dst + len_src);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i++);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (i <= (int)ft_strlen(s))
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
