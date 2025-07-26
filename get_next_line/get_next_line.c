7c3e4debc8a5dfdff3a0332d5d38e9caf40dacbd
Dennis <dennis.wt20@gmail.com>
2025-07-26 13:58:26 +0200

    main

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 13:01:06 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/07/25 16:59:06 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/07/25 20:58:11 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2);
 
 // 1. get_next_line call
 // rest leer
@@ -21,6 +23,25 @@
 // wenn ja: in line speichern und zurückgeben
 // wenn nein: wieder buffer liest und addiert zu rest dazu
 // bis ende
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_ptr;
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*ptr;
+
+	len_ptr = ft_strlen(s1) + ft_strlen(s2);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	ptr = (char *)malloc((len_ptr + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, s1, len_s1 + 1);
+	ft_strlcat(ptr + len_s1, s2, len_s2 + 1);
+	ptr[len_ptr + 1] = '\0';
+	return (ptr);
+}
 
 char	*get_next_line(int fd)
 {
---
+++ get_next_line/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 20:58:27 by dwotsche          #+#    #+#             */
+/*   Updated: 2025/07/25 21:17:40 by dwotsche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int	fd = open(test.txt, O_READONLY);
+	get_next_line(fd);
+	return (0);
+}
